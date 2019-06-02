3d0e5973e2c6591cd2368c876a0a6596aa3b59c4
Paweł Nowak <kontakt.nowakpawel@gmail.com>
2019-06-02 02:48:03 +0200

    setting tab title - needs to be fixed

--- main.py
+++ main.py
@@ -80,11 +80,13 @@ class App(QFrame):
 
         self.tabs.append(QWidget())
         self.tabs[i].layout = QVBoxLayout()
-        self.tabs[i].setObjectName("tab " + str(i))
+        self.tabs[i].setObjectName("tab" + str(i))
 
         # Open web view
         self.tabs[i].content = QWebEngineView()
         self.tabs[i].content.load(QUrl.fromUserInput("http://www.google.com"))
+
+        self.tabs[i].content.titleChanged.connect(lambda: self.setTabText(i))
 
         # Add web view to tabs layout
         self.tabs[i].layout.addWidget(self.tabs[i].content)
@@ -97,7 +99,8 @@ class App(QFrame):
         self.container.layout.setCurrentWidget(self.tabs[i])
 
         self.tabbar.addTab("New Tab")
-        self.tabbar.setTabData(i, "tab " + str(i))
+        self.tabbar.setTabData(i, {"object": "tab" + str(i), "initial": i})
+
         self.tabbar.setCurrentIndex(i)
 
         self.tabCounter += 1
@@ -114,7 +117,7 @@ class App(QFrame):
         address = self.addressBar.text()
 
         i = self.tabbar.currentIndex()
-        tab = self.tabbar.tabData(i)
+        tab = self.tabbar.tabData(i)["object"]
         wv = self.findChild(QWidget, tab).content
 
         if "http" not in address:
@@ -131,8 +134,30 @@ class App(QFrame):
 
         self.addressBar.setText(url)
 
+    def setTabText(self, i):
+        tab_name = self.tabs[i].objectName()
+
+        print(tab_name)
+
+        count = 0
+        running = True
+
+        while running:
+            tab_data_name = self.tabbar.tabData(count)
+
+            if count >= 99:
+                running = False
+
+            if tab_name == tab_data_name["object"]:
+                newTitle = self.findChild(QWidget, tab_name).content.title()
+                self.tabbar.setTabText(count, newTitle)
+                running = False
+            else:
+                count += 1
+
     def closeTab(self, i):
         self.tabbar.removeTab(i)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
